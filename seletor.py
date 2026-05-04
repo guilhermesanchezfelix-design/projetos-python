1b7266684603d12e7c12e72047aceeab751f9863
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-05-04 09:33:11 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de caracteres--
+letra = input("Digite a letra")
+if letra == "A":
+  print("Voce escolheu a letra "B")
+elif letra == "B":
+   print("Voce escolkheu a leitura "B")
+else:
+   print("Essa opção não esta disponivel")
