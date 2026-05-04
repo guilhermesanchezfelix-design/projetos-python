c8b0fd867e0539b84a5d2dd3f160a45e9e3c2dcb
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-05-04 14:20:36 +0000

    v3

--- seletor.py
+++ seletor.py
@@ -1,8 +1,10 @@
-#--Seletor de caracteres--
-letra = input("Digite a letra")
+print("sistema seletor de catacteres 🔠")
+print("Escolha uma letra e veja se ela esta disponivel👀")
+letra = input("Digite a letra: ").upper()
 if letra == "A":
-  print("Voce escolheu a letra "B")
+  print("Voce escolheu a letra A 👌")
 elif letra == "B":
-   print("Voce escolkheu a leitura "B")
+  print("Voce escolheu a letra B 👌")
 else:
-   print("Essa opção não esta disponivel")
+  print("Essa opção não esta disponivel ❌")
+  
