c81c7561801b8f0badb38a79fc7890ef09434548
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-05-24 19:14:22 +0000

    meu codigo

---
+++ simulador_investimento_cdb.py
@@ -0,0 +1,18 @@
+#Simulador De Investimento - CDB--
+
+deposito = float(input("Digite o valor do aporte:"))
+taxa = float(input("Qual a taxa do CDB em % ?"))
+meses = int(input("Quantos meses vai investir?"))
+
+conversao = taxa/100
+
+total = 0
+
+for mes in range(1, meses +1):
+
+    total = total + deposito
+    total + total + (total * conversao)
+
+print(f"Ao final do periodo, voce tera:R${total:.2f}")
+
+#Guilherme Sanchez Felix
