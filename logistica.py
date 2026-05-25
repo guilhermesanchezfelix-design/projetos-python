d5b33b34ee44921d23b80404fe24d9b448ccbbfa
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-05-25 16:01:11 +0000

    v1

---
+++ logistica.py
@@ -0,0 +1,12 @@
+#Calculador de Frete --
+def cabecalho():
+    print("\n" + "=" *30)
+
+    print("SISTEMA DE LOGISTICA")
+def calcular_frete(peso):
+    if peso >= 20 and peso > 20:
+        return peso * 10
+    else:
+        return peso * 15
+    
+
