9a6ba8a81aa538e19ac9719711524c09291a9eb6
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-05-26 15:49:05 +0000

    v1

--- logistica.py
+++ logistica.py
@@ -4,9 +4,12 @@ def cabecalho():
 
     print("SISTEMA DE LOGISTICA")
 def calcular_frete(peso):
-    if peso >= 20 and peso > 20:
-        return peso * 10
+    if peso_carga >= 20 : 
+        return peso_carga * 10.00
     else:
-        return peso * 15
-    
-
+        return peso_carga * 15.00
+cabecalho()
+peso_carga = float(input("Digite o peso da carga em (kg):"))
+frete = calcular_frete(peso_carga)
+print(F"O valor do frete é: R$ {frete:.2f}")   
+print("=" * 30)
