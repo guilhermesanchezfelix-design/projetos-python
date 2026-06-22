7351f2f596c07e9d1bb0bf366c8c54346376252c
guilhermesanchezfelix-design <guilhermesanchezfelix@gmail.com>
2026-06-22 14:04:09 +0000

    v1

--- playlist.py
+++ playlist.py
@@ -2,6 +2,7 @@ print("Music player🎵")
 playlist = ["Musica 1 - gospel","Musica 2 - Sertanejo","Musica 3 - Internacional"]
 atual = int(input("Digite o numero da Musica"))
 indice = atual -1
+
 print (f"Tocando Agora :{playlist[indice]}")
 print("Musica Amterior: --(Inicio Da Playlist)")
 print("=" * 40)
---
+++ podio.py
@@ -0,0 +1,5 @@
+vencedor = ["Lucas", "Davi", "Raphaela"]
+
+print("🥇 ouro:", vencedor [0])
+print("🥈 prata:", vencedor [1])
+print("🥉 bronze:", vencedor [2])
